d033749034e15d9cccd3fb103d79a2b939486c4b
masaun <masaun@protonmail.com>
2025-10-28 15:28:25 -0700

    [SC test]: Add the OpenbandsV2HyperlaneIntegrationOnLocal.t.sol

---
+++ contracts/test/hyperlane/local/run-test-on-local.sh
@@ -0,0 +1,2 @@
+echo "Running the OpenbandsV2 contracts and the Hyperlane wrapper contract integration tests on local environment..."
+forge test ./test/hyperlane/local/OpenbandsV2HyperlaneIntegrationOnLocal.t.sol
